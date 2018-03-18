93de8abef7667e7864296928eeb0d008f1a70e7d
ChenXiaoMian <236273522@qq.com>
2018-03-18 22:08:13 +0800

    remove

--- KMInfoCollect/src/js/page/historyData.js
+++
@@ -1,9 +0,0 @@
-// 历史记录逻辑
-$(function(){
-    store.remove('chooseHistory');
-    $("#historyType a").on('click',function(){
-        var type = $(this).data("type");
-        store.set('chooseHistory',type);
-        window.pageManager.go('historyList');
-    });
-});
--- KMInfoCollect/src/js/page/tempData.js
+++
@@ -1,9 +0,0 @@
-// 选择模板类型逻辑
-$(function(){
-    store.remove('chooseTemp');
-    $("#tempType a").on('click',function(){
-        var type = $(this).data("type");
-        store.set('chooseTemp',type);
-        window.pageManager.go('tempList');
-    });
-});
